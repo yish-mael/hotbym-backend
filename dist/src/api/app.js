9f99a199ae9e945f80805a39ae0c811b8743cbb6
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2022-11-23 20:21:33 +0100

    fix: cors

--- dist/src/api/app.js
+++ dist/src/api/app.js
@@ -20,7 +20,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 //connect();
 const app = (0, express_1.default)();
 // app.use(cors({credentials: true, origin: ["https://hotbym.com", /\.hotbym\.com$/, "http://localhost:3000", "http://localhost:3001", "https://yrvcnztcqm.us-east-1.awsapprunner.com" ]}));
-app.use((0, cors_1.default)({ credentials: true, origin: true }));
+app.use((0, cors_1.default)({ credentials: true, origin: "*" }));
 const swaggerOptions = {
     swaggerDefinition: {
         openapi: '3.0.0',
--- src/api/app.ts
+++ src/api/app.ts
@@ -20,7 +20,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 // app.use(cors({credentials: true, origin: ["https://hotbym.com", /\.hotbym\.com$/, "http://localhost:3000", "http://localhost:3001", "https://yrvcnztcqm.us-east-1.awsapprunner.com" ]}));
-app.use(cors({credentials: true, origin: true}));
+app.use(cors({credentials: true, origin: "*"}));
 
 const swaggerOptions = {
     swaggerDefinition: {
