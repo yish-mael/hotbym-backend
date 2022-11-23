956abacfca286a98d27414f64898c82bfb3abaa3
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2022-11-23 16:33:02 +0100

    fix: cors

--- dist/src/api/app.js
+++ dist/src/api/app.js
@@ -19,7 +19,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 // }
 //connect();
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ credentials: true, origin: ["https://hotbym.com", /\.hotbym\.com$/, "http://localhost:3000", "http://localhost:3001"] }));
+app.use((0, cors_1.default)({ credentials: true, origin: ["https://hotbym.com", /\.hotbym\.com$/, "http://localhost:3000", "http://localhost:3001", "https://yrvcnztcqm.us-east-1.awsapprunner.com"] }));
 // app.use(cors({credentials: true, origin: true}));
 const swaggerOptions = {
     swaggerDefinition: {
--- src/api/app.ts
+++ src/api/app.ts
@@ -19,7 +19,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({credentials: true, origin: ["https://hotbym.com", /\.hotbym\.com$/, "http://localhost:3000", "http://localhost:3001" ]}));
+app.use(cors({credentials: true, origin: ["https://hotbym.com", /\.hotbym\.com$/, "http://localhost:3000", "http://localhost:3001", "https://yrvcnztcqm.us-east-1.awsapprunner.com" ]}));
 // app.use(cors({credentials: true, origin: true}));
 
 const swaggerOptions = {
